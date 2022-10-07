1492fe21370569aae41f8f24a797b10a8a8aa0c1
YihaoSu <yhsu@astro.ncu.edu.tw>
2022-10-07 18:52:20 +0800

    調整欄位名稱

--- utilities/__init__.py
+++ utilities/__init__.py
@@ -14,7 +14,7 @@ def get_gw_event_table_by_gwpy():
     gw_event_table = gw_event_table.rename(
         columns={
             'commonName': '事件名稱',
-            'GPS': '緻密星體合併事件發生的時間(GPS時間系統)',
+            'GPS': '觀測到緻密星體合併事件的時間(GPS時間系統)',
             'mass_1_source': '緻密星體1的質量(單位：太陽質量)',
             'mass_2_source': '緻密星體2的質量(單位：太陽質量)',
             'luminosity_distance': '與地球的距離(單位：百萬秒差距)',
