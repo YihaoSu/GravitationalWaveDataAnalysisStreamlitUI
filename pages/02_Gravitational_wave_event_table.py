cac35f980e1a69ddc7114128e34d798a28b7a5ea
YihaoSu <yhsu@astro.ncu.edu.tw>
2022-10-08 11:04:40 +0800

    新增將GPS時間轉成UTC時間的功能

--- pages/02_Gravitational_wave_event_table.py
+++ pages/02_Gravitational_wave_event_table.py
@@ -2,7 +2,8 @@ import streamlit as st
 from st_aggrid import AgGrid, GridOptionsBuilder
 
 from utilities import (
-    get_gw_event_table_by_gwpy
+    get_gw_event_table_by_gwpy,
+    convert_gps_to_utc
 )
 
 
@@ -14,6 +15,16 @@ st.info('藉由[GWpy](https://gwpy.github.io/docs/stable/)套件取得[重力波
 with st.spinner('正在載入重力波事件列表，請稍候...'):
     gw_event_table = get_gw_event_table_by_gwpy()
 
+time_format = st.radio(
+    '切換表格中的時間格式', ['GPS時間系統', '世界協調時間(UTC)'], horizontal=True
+)
+if time_format == '世界協調時間(UTC)':
+    gw_event_table = convert_gps_to_utc(gw_event_table)
+else:
+    gw_event_table = gw_event_table.rename(
+        columns={'GPS': '觀測到緻密星體合併事件的時間(GPS時間系統)'}
+    )
+st.markdown('[GPS時間系統](https://gwpy.github.io/docs/stable/time/)是指從1980年1月6日午夜開始起算的秒數，重力波開放科學中心網站的「[UTC/GPS Time Converter](https://www.gw-openscience.org/gps/)」頁面有提供將GPS時間系統轉換成[世界協調時間(UTC)](https://zh.wikipedia.org/zh-tw/%E5%8D%8F%E8%B0%83%E4%B8%96%E7%95%8C%E6%97%B6)的功能。')
 
 gb = GridOptionsBuilder.from_dataframe(gw_event_table)
 gb.configure_column('事件名稱', pinned='left')
--- utilities/__init__.py
+++ utilities/__init__.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from gwpy.table import EventTable
+from gwpy.time import from_gps
 
 
 @st.experimental_memo(ttl=86400, show_spinner=False)
@@ -10,15 +11,25 @@ def get_gw_event_table_by_gwpy():
             'commonName', 'GPS', 'mass_1_source', 'mass_2_source', 'luminosity_distance'
         ]
     ]
-    gw_event_table.sort_values(by='GPS', inplace=True, ignore_index=True)
     gw_event_table = gw_event_table.rename(
         columns={
             'commonName': '事件名稱',
-            'GPS': '觀測到緻密星體合併事件的時間(GPS時間系統)',
             'mass_1_source': '緻密星體1的質量(單位：太陽質量)',
             'mass_2_source': '緻密星體2的質量(單位：太陽質量)',
             'luminosity_distance': '與地球的距離(單位：百萬秒差距)',
         }
+    )
+    gw_event_table.sort_values(by='GPS', inplace=True, ignore_index=True)
+
+    return gw_event_table
+
+
+def convert_gps_to_utc(gw_event_table):
+    gw_event_table['GPS'] = gw_event_table['GPS'].apply(
+        lambda x: from_gps(x).strftime('%Y-%m-%d %H:%M:%S')
+    )
+    gw_event_table = gw_event_table.rename(
+        columns={'GPS': '觀測到緻密星體合併事件的時間(UTC)'}
     )
 
     return gw_event_table
