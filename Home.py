28e301186790f69ffdef18328153462d9f5b96e6
YihaoSu <yhsu@astro.ncu.edu.tw>
2022-10-09 20:12:17 +0800

    在重力波事件統計頁面上呈現緻密星體質量分佈圖

--- Home.py
+++ Home.py
@@ -7,3 +7,4 @@ st.title(page_title)
 
 st.info('此Web App是以重力波資料分析作為範例，用來教學及推廣公民科學，由[蘇羿豪](https://astrobackhacker.tw/)基於[Streamlit](https://streamlit.io/)開發，[程式碼](https://github.com/YihaoSu/GravitationalWaveDataAnalysisStreamlitUI)以MIT授權條款開源，並將開發過程紀錄在2022 iThome鐵人賽的系列文章「[跟著黑蛋用Streamlit速成天文資料分析Web App](https://ithelp.ithome.com.tw/users/20103436/ironman/5820)」中。')
 st.markdown('* Gravitational wave event table頁面呈現從[重力波開放科學中心](https://www.gw-openscience.org/)取得的重力波事件列表，並提供匯出CSV或JSON檔的功能。')
+st.markdown('* Gravitational wave statistical charts頁面呈現重力波事件的統計圖。')
--- utilities/__init__.py
+++ utilities/__init__.py
@@ -8,14 +8,20 @@ def get_gw_event_table_by_gwpy():
     gw_event_table = EventTable.fetch_open_data('GWTC').to_pandas()
     gw_event_table = gw_event_table[
         [
-            'commonName', 'GPS', 'mass_1_source', 'mass_2_source', 'luminosity_distance'
+            'commonName', 'GPS', 'mass_1_source', 'mass_2_source',
+            'final_mass_source', 'luminosity_distance'
         ]
     ]
+    gw_event_table['mass_1_source'] = gw_event_table['mass_1_source'].astype(float)
+    gw_event_table['mass_2_source'] = gw_event_table['mass_2_source'].astype(float)
+    gw_event_table['final_mass_source'] = gw_event_table['final_mass_source'].astype(float)
+    gw_event_table['luminosity_distance'] = gw_event_table['luminosity_distance'].astype(float)
     gw_event_table = gw_event_table.rename(
         columns={
             'commonName': '事件名稱',
             'mass_1_source': '緻密星體1的質量(單位：太陽質量)',
             'mass_2_source': '緻密星體2的質量(單位：太陽質量)',
+            'final_mass_source': '緻密星體合併後的質量(單位：太陽質量)',
             'luminosity_distance': '與地球的距離(單位：百萬秒差距)',
         }
     )
